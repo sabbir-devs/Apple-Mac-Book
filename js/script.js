a647781243aac94c432964eac6e2ad77d495eb9a
sabbir322 <sabbirhosan370@gmail.com>
2021-09-26 22:58:28 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var memoryArTaka = document.getElementById('memory-ar-taka');
+var ssdArTaka = document.getElementById('ssd-ar-taka');
+var baritaDeliveryDeoarTaka = document.getElementById('barita-delivary-deoar-taka');
+
+function basiTaka(pochondo){
+    if(pochondo === 'olpo-memory'){
+        memoryArTaka.innerText = 0;
+    }
+    else if(pochondo === '16GB-memory'){
+        memoryArTaka.innerText = 180;
+    }
+    else if(pochondo === 'olpo-ssd'){
+        ssdArTaka.innerText = 0;
+    }
+    else if(pochondo === '521GB-ssd'){
+        ssdArTaka.innerText = 100;
+    }
+    else if(pochondo === '1TB-ssd'){
+        ssdArTaka.innerText = 180;
+    }
+    else if(pochondo === 'mangna-dari-koira-delivery'){
+        baritaDeliveryDeoarTaka.innerText = 0;
+    }
+    else if(pochondo === 'taka-diya-taratai-delivery'){
+        baritaDeliveryDeoarTaka.innerText = 20;
+    }
+    sob()
+    
+}
+
+var puraTakaBeforeSobkisu = document.getElementById('pura-taka-before-sobkisu');
+var puraTakaAfterSobkisu = document.getElementById('pura-taka-after-sobkisu');
+function sob(){
+    var aroTakaAroTaka = parseInt(memoryArTaka.innerText) + parseInt(ssdArTaka.innerText) + parseInt(baritaDeliveryDeoarTaka.innerText);
+    puraTakaBeforeSobkisu.innerText = 1299 + aroTakaAroTaka;
+    puraTakaAfterSobkisu.innerText = 1299 + aroTakaAroTaka;
+}
+
+
+// document.getElementById('promo-apply').addEventListener('click', function(){
+//     var takaBacao = document.getElementById('promo-input');
+//     var amiKipta = document.getElementById('pura-taka-after-sobkisu');
+//     if(takaBacao.value === peskuthedon){
+//         puraTakaAfterSobkisu.innerText = amiKipta - (amiKipta * 100 / 25);
+//     }
+//     amiKipta.value = '';
+// })
